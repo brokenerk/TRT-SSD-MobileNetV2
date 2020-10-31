5387058de0163a17ab1a7c9617e03ae127e28f05
brokenerk <enriquebroly@hotmail.com>
2020-10-31 16:36:42 -0600

    SSD working with TensorRT on Nvidia Jetson AGX Xavier

---
+++ README.md
@@ -0,0 +1,105 @@
+TensorRT Python Sample for Object Detection
+======================================
+
+Performance includes memcpy and inference.
+</br>
+
+| Model | Input Size | TRT Nano |
+|:------|:----------:|-----------:|
+| ssd_inception_v2_coco(2017) | 300x300 | 49ms |
+| ssd_mobilenet_v1_coco | 300x300 | 36ms |
+| ssd_mobilenet_v2_coco | 300x300 | 46ms |
+
+Since the optimization of preprocessing is not ready yet, we don't include image read/write time here.
+</br>
+</br>
+
+## Install dependencies
+
+```C
+$ sudo apt-get install python3-pip libhdf5-serial-dev hdf5-tools
+$ pip3 install --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v42 tensorflow-gpu==1.13.1+nv19.5 --user
+$ pip3 install numpy pycuda --user
+```
+
+</br>
+</br>
+
+## Download model
+
+Please download the object detection model from <a href=https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/detection_model_zoo.md>TensorFlow model zoo</a>.
+</br>
+
+```C
+$ git clone https://github.com/AastaNV/TRT_object_detection.git
+$ cd TRT_object_detection
+$ mkdir model
+$ cp [model].tar.gz model/
+$ tar zxvf model/[model].tar.gz -C model/
+```
+
+##### Supported models:
+
+- ssd_inception_v2_coco_2017_11_17
+- ssd_mobilenet_v1_coco
+- ssd_mobilenet_v2_coco
+
+We will keep adding new model into our supported list.
+
+</br>
+</br>
+
+## Update graphsurgeon converter
+
+Edit /usr/lib/python3.6/dist-packages/graphsurgeon/node_manipulation.py
+
+```C
+diff --git a/node_manipulation.py b/node_manipulation.py
+index d2d012a..1ef30a0 100644
+--- a/node_manipulation.py
++++ b/node_manipulation.py
+@@ -30,6 +30,7 @@ def create_node(name, op=None, _do_suffix=False, **kwargs):
+     node = NodeDef()
+     node.name = name
+     node.op = op if op else name
++    node.attr["dtype"].type = 1
+     for key, val in kwargs.items():
+         if key == "dtype":
+             node.attr["dtype"].type = val.as_datatype_enum
+```
+</br>
+</br>
+
+## RUN
+
+**1. Maximize the Nano performance**
+```C
+$ sudo nvpmodel -m 0
+$ sudo jetson_clocks
+```
+</br>
+
+**2. Update main.py based on the model you used**
+```C
+from config import model_ssd_inception_v2_coco_2017_11_17 as model
+from config import model_ssd_mobilenet_v1_coco_2018_01_28 as model
+from config import model_ssd_mobilenet_v2_coco_2018_03_29 as model
+```
+</br>
+
+**3. Execute**
+```C
+$ python3 main.py [image]
+```
+
+It takes some time to compile a TensorRT model when the first launching.
+</br>
+After that, TensorRT engine can be created directly with the serialized .bin file
+</br>
+</br>
+@ To get more memory, it's recommended to turn-off X-server.
+</br>
+</br>
+</br>
+</br>
+</br>
---
+++ config.py
@@ -0,0 +1,96 @@
+#https://www.minds.ai/post/deploying-ssd-mobilenet-v2-on-the-nvidia-jetson-and-nano-platforms
+import graphsurgeon as gs
+import numpy as np
+
+path = 'model/frozen_graph.pb'
+TRTbin = 'TRT_ssd_mobilenet_v2_coco_2018_03_29.bin'
+output_name = ['NMS']
+dims = [3,450,450]
+layout = 7
+
+def add_plugin(graph):
+    all_assert_nodes = graph.find_nodes_by_op("Assert")
+    graph.remove(all_assert_nodes, remove_exclusive_dependencies=True)
+
+    all_identity_nodes = graph.find_nodes_by_op("Identity")
+    graph.forward_inputs(all_identity_nodes)
+
+    Input = gs.create_plugin_node(
+        name="Input",
+        op="Placeholder",
+        shape=[1, 3, 450, 450]
+    )
+
+    PriorBox = gs.create_plugin_node(
+        name="GridAnchor",
+        op="GridAnchor_TRT",
+        minSize=0.2,
+        maxSize=0.95,
+        aspectRatios=[1.0, 2.0, 0.5, 3.0, 0.33],
+        variance=[0.1,0.1,0.2,0.2],
+        #featureMapShapes=[19, 10, 5, 3, 2, 1], # Resolution 300
+        featureMapShapes=[29, 15, 8, 4, 2, 1], # Resolution 450
+        numLayers=6
+    )
+
+    NMS = gs.create_plugin_node(
+        name="NMS",
+        op="NMS_TRT",
+        shareLocation=1,
+        varianceEncodedInTarget=0,
+        backgroundLabelId=0,
+        confidenceThreshold=1e-8,
+        nmsThreshold=0.6,
+        topK=100,
+        keepTopK=100,
+        numClasses=2,
+        inputOrder=[0, 2, 1],
+        confSigmoid=1,
+        isNormalized=1
+    )
+
+    concat_priorbox = gs.create_node(
+        "concat_priorbox",
+        op="ConcatV2",
+        axis=2
+    )
+
+    concat_box_loc = gs.create_plugin_node(
+        "concat_box_loc",
+        op="FlattenConcat_TRT",
+        axis=1,
+        ignoreBatch=0
+    )
+
+    concat_box_conf = gs.create_plugin_node(
+        "concat_box_conf",
+        op="FlattenConcat_TRT",
+        axis=1,
+        ignoreBatch=0
+    )
+
+    namespace_plugin_map = {
+        "MultipleGridAnchorGenerator": PriorBox,
+        "Postprocessor": NMS,
+        "Preprocessor": Input,
+        "Cast": Input,
+        "image_tensor": Input,
+        "Concatenate": concat_priorbox,
+        "concat": concat_box_loc,
+        "concat_1": concat_box_conf
+    }
+
+    for node in graph.find_nodes_by_op('FusedBatchNormV3'):
+        gs.update_node(node, op='FusedBatchNorm')
+
+    graph.collapse_namespaces(namespace_plugin_map)
+    graph.remove(graph.graph_outputs, remove_exclusive_dependencies=False)
+    graph.find_nodes_by_op("NMS_TRT")[0].input.remove("Input")
+
+    # Create a constant Tensor and set it as input for GridAnchor_TRT
+    data = np.array([1, 1], dtype=np.float32) 
+    anchor_input = gs.create_node("AnchorInput", "Const", value=data)  
+    graph.append(anchor_input)
+    graph.find_nodes_by_op("GridAnchor_TRT")[0].input.insert(0, "AnchorInput")
+
+    return graph
